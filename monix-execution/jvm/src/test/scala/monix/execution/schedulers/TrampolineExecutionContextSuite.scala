ca8d83733522e8678b210d29937e6cac0168d6a0
Mikołaj Bul <m.bul@avsystem.com>
2025-07-11 10:15:55 +0200

    improve test

--- monix-execution/jvm/src/test/scala/monix/execution/schedulers/TrampolineExecutionContextSuite.scala
+++ monix-execution/jvm/src/test/scala/monix/execution/schedulers/TrampolineExecutionContextSuite.scala
@@ -61,14 +61,16 @@ object TrampolineExecutionContextSuite extends SimpleTestSuite {
     final class TestException extends NoStackTrace
 
     val timeoutMillis = 5000
-    val didTimeout = new AtomicBoolean(false)
+    val didTimeoutOrFail = new AtomicBoolean(false)
     // 32 to fill 2 chunks of ChunkedArrayQueue, trying to expose a stale reference (e.g. headArray)
     val totalTasks = 32
     val tasksCounter = new AtomicInteger(0)
 
     def ignoreTestExceptions: UncaughtExceptionReporter = {
       case _: TestException =>
-      case x => x.printStackTrace()
+      case x =>
+        didTimeoutOrFail.set(true)
+        x.printStackTrace()
     }
 
     val context = TrampolineExecutionContext(Scheduler.io(
@@ -88,7 +90,7 @@ object TrampolineExecutionContextSuite extends SimpleTestSuite {
         // tasks used to get stuck / be skipped in the trampoline run loop,
         // trying to detect it with a timeout
         println(s"Timeout reached, only ${tasksCounter.get()} tasks executed out of $totalTasks")
-        didTimeout.set(true)
+        didTimeoutOrFail.set(true)
       }
     }
 
@@ -109,6 +111,9 @@ object TrampolineExecutionContextSuite extends SimpleTestSuite {
     (1 to 1000).foreach { _ =>
       executeNestedTrampoline()
     }
-    assert(!didTimeout.get(), "Executions inside the trampoline should not timeout")
+    assert(
+      !didTimeoutOrFail.get(),
+      "Executions inside the trampoline should not timeout, nor throw unexpected exceptions"
+    )
   }
 }
